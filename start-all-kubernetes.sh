530f4b1356020d434f1bdded92c60706905cc35f
Didier Hoarau <didier.hoarau.dev@gmail.com>
2019-02-22 20:08:35 +0800

    Fix final services to deploy

--- start-all-kubernetes.sh
+++ start-all-kubernetes.sh
@@ -5,14 +5,15 @@ set -e
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${ROOT_DIR}
 
-kubectl get all \
-    | grep pets \
-    | grep -v replicaset.apps \
-    | grep -v pod \
-    | cut -f1 -d" " \
-    | xargs kubectl delete > /dev/null || true
+# To Reset all services
+# kubectl get all \
+#     | grep pets \
+#     | grep -v replicaset.apps \
+#     | grep -v pod \
+#     | cut -f1 -d" " \
+#     | xargs kubectl delete > /dev/null || true
 
-sleep 5
+# sleep 5
 
 function startService {
     echo ''
@@ -30,3 +31,4 @@ startService pets-dogs
 startService pets-hamsters
 startService pets-parrots
 startService pets-turtles
+startService pets-comments
